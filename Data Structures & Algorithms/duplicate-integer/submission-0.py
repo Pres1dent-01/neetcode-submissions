724ce505970f7579f609f049a6ac114fa59d5aa4
Vale <51172366+Pres1dent-01@users.noreply.github.com>
2026-09-09 16:32:49 +0100

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hash={}
+        for num in nums:
+            if hash.get(num, 0) >= 1:
+                return True
+            hash[num] = hash.get(num, 0)+1
+        return False
